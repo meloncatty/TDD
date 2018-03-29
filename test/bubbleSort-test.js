ca1784a9c78dca5d5f4c31985c98c3ccb7537bef
meloncatty <k.handel66@gmail.com>
2018-03-29 09:18:27 -0600

    add tests and clean up wording Added a few more tests to mergeSort, insertionSort, bubbleSort and change all tests containing the word number to say integer instead

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -1,31 +1,52 @@
-const chai = require('chai');
-require('locus');
-const expect = chai.expect;
-const assert = chai.assert;
-const bubbleSort = require('../lib/bubbleSort.js');
+const chai = require('chai')
+require('locus')
+const expect = chai.expect
+const assert = chai.assert
+const bubbleSort = require('../lib/bubbleSort.js')
 const generateArray = require('../lib/generateArray.js')
 
 describe('bubbleSort', () => {
   it('should be a funciton', () => {
-    assert.isFunction(bubbleSort);
-  });
+    assert.isFunction(bubbleSort)
+  })
 
-  it('should sort an array of two numbers', () => {
-    const newArray = [3, 1];
-    bubbleSort(newArray);
-    assert.deepEqual(newArray, [1, 3]);
-  });
+  it('should sort an array of two integers', () => {
+    const newArray = [3, 1]
+    bubbleSort(newArray)
+    assert.deepEqual(newArray, [1, 3])
+  })
 
-  it('should sort an array of four numbers', () => {
-    const newArray = [22, 9, 40, 4];
-    bubbleSort(newArray);
-    assert.deepEqual(newArray, [4, 9, 22, 40]);
-  });
+  it('should sort an array of four integers', () => {
+    const newArray = [22, 9, 40, 4]
+    bubbleSort(newArray)
+    assert.deepEqual(newArray, [4, 9, 22, 40])
+  })
 
-  it('should sort an array of n amount of numbers', () => {
-    const newArray = generateArray(40);
-    const sortedArray = bubbleSort([...newArray]);
+  it('should sort an array of positive and negative integers', () => {
+    const newArray = [-40, -33, 6, 12, 88, -100]
+    const sortedArray = bubbleSort(newArray)
 
-    assert.deepEqual(sortedArray, newArray.sort((a,b) => a - b));
+    assert.deepEqual(sortedArray, [-100, -40, -33, 6, 12, 88])
+  })
+
+  it('should sort letters alphabetically', () => {
+    const newArray = ['s', 'z', 'a', 't', 'g']
+    const sortedArray = bubbleSort(newArray)
+
+    assert.deepEqual(sortedArray, ['a', 'g', 's', 't', 'z'])
+  })
+
+  it('should sort an array of 5000 amount of integers', () => {
+    const newArray = generateArray(5000)
+    const sortedArray = bubbleSort([...newArray])
+
+    assert.deepEqual(sortedArray, newArray.sort((a,b) => a - b))
+  })
+
+  it('should sort an array of 10000 amount of integers', () => {
+    const newArray = generateArray(10000)
+    const sortedArray = bubbleSort([...newArray])
+
+    assert.deepEqual(sortedArray, newArray.sort((a,b) => a - b))
   })
 })
